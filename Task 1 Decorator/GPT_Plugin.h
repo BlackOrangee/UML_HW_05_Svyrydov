2656f4d33daa0f45b7fbda0fe01fd157cec0714c
BlackOrangee <doktorzoidberg8@gmail.com>
2023-11-05 12:35:09 +0200

    Decorator

---
+++ Task 1 Decorator/GPT_Plugin.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "TextEditor.h"
+class GPT_Plugin :
+    public TextEditor
+{
+public:
+    GPT_Plugin() {};
+    GPT_Plugin(TextEditor*);
+    void write(TextEditor*) override;
+    string read();
+
+};
+
---
+++ Task 1 Decorator/Task 1 Decorator.cpp
@@ -0,0 +1,20 @@
+﻿// Task 1 Decorator.
+// 
+// Develop a simple text editor with the ability to add various plugins (decorators) to extend the editor's functionality.
+//
+
+#include <iostream>
+#include "TextEditor.h"
+#include "GPT_Plugin.h"
+
+using namespace std;
+
+int main()
+{
+	TextEditor* text = new TextEditor("text");
+
+	TextEditor* GPTText = new GPT_Plugin(text);
+
+
+	cout << GPTText->read();
+}
